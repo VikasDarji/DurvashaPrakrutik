75e24964e98ff754d6ddce885b6b902ba6c7f752
VikasDarji <vikasdarji052@gmail.com>
2025-11-06 12:44:23 +0530

    v0605

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import "./Navbar.css"
 function Navbar() {
   return (
     <nav>
-    
+    <h1 className="logi">Welcome to our Website    </h1>
+    <li className="link">
       <Link to="/" style={{ textDecoration: "none", color: "#333" }}>
         Home
       </Link>
@@ -15,6 +16,7 @@ function Navbar() {
       <Link to="/contact" style={{ textDecoration: "none", color: "#333" }}>
         Contact Us
       </Link>
+      </li>
     </nav>
   );
 }
