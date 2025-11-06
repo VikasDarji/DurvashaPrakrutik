81ef81b4fda541a1e3c5f69068a984a8d37fc4b9
abhibs1508-crypto <abhibs1508@gmail.com>
2025-11-06 12:18:22 +0530

    a0603

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <nav
-      style={{
-        display: "flex",
-        gap: "20px",
-        padding: "10px 20px",
-        backgroundColor: "#f2f2f2",
-        borderBottom: "2px solid #ccc",
-      }}
-    >
+    <nav>
+    
       <Link to="/" style={{ textDecoration: "none", color: "#333" }}>
         Home
       </Link>
