46c1a681e54f14d143a789a37bdbdfe1477bd36a
VikasDarji <vikasdarji052@gmail.com>
2025-11-07 09:28:37 +0530

    v0607

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <div className="home-content">
         <h1>Welcome to MyBusiness</h1>
         <p>We help you grow with innovation, trust, and technology.</p>
-        <button className="btn-primary">Get Started</button>
+     <a class="inline-block rounded-2xl bg-blue-600 px-8 py-4 text-white text-base font-medium shadow-md transition hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600" href="/contact">Get In Touch</a>
       </div>
     </section>
   );
